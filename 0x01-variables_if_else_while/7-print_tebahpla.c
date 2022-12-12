65d7a74dd78c85b26a5496ea75b76cb30af562e7
imonisweet1 <isaacimonivwerha@gmail.com>
2022-12-11 18:17:31 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+* main - prints all single digit number of base 10
+* starting from 0, followed by a new line
+* Return: Always 0
+*/
+in main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
