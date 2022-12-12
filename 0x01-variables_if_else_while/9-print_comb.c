78fd1569403fa8577f41f6322975ff1a159b7e44
imonisweet1 <isaacimonivwerha@gmail.com>
2022-12-11 18:51:05 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - prints all single digit number in base 10
+* starting from 0, followed by a new line.
+* Return: Always 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+		return (0);
+}
