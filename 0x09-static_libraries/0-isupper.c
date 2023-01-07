98c1230714f0eed621aa03b0831730f0ec56fcce
imonisweet1 <isaacimonivwerha@gmail.com>
2023-01-06 21:50:03 -0800

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "main.h"
+/**
+ * _isupper - checks if parameter is an uppercase character
+ * @c: input character
+ * Return: 1 if is an uppercase character, otherwise 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
