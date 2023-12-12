73b048a32b3c9782ae7fc4b488a023319d0c2646
Isaac Imonivwerha <isaacimonivwerha@gmail.com>
2023-12-12 05:16:20 -0800

    task 01

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,13 +9,21 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-        size_t nodes = 0;
+	int count;
 
-        while (h)
-        {
-                nodes++;
-                h = h->next;
-        }
+	count = 0;
 
-        return (nodes);
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
 }
