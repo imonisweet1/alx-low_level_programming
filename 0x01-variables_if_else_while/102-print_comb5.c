4471d37fe071bd2d55c19cd1f34ac02a7b3f07c7
Isaac Imonivwerha <isaacimonivwerha@gmail.com>
2023-12-23 03:50:06 -0800

    tasks 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,35 +6,24 @@
  */
 int main(void)
 {
-	int num1_tens, num1_units, num2_tens, num2_units;
+	int num1, num2;
 
-	for (num1_tens = 0; num1_tens < 10; num1_tens++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (num1_units = 0; num1_units < 10; num1_units++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			for (num2_tens = 0; num2_tens < 10; num2_tens++)
-			{
-				for (num2_units = 0; num2_units < 10; num2_units++)
-				{
-					if (num2_tens > num1_tens || (num2_tens == num1_tens && num2_units >= num1_units))
-					{
-						putchar(num1_tens + '0');
-						putchar(num1_units + '0');
-						putchar(' ');
-						putchar(num2_tens + '0');
-						putchar(num2_units + '0');
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-						if (!(num1_tens == 9 && num1_units == 9 && num2_tens == 9 && num2_units == 9))
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-				}
-			}
+			if (num1 == 98 && num2 == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
-
 	putchar('\n');
 
 	return (0);
