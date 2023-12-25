9c219aedb3cc18f9dc181e1efad7990f491b55ea
Isaac Imonivwerha <isaacimonivwerha@gmail.com>
2023-12-25 15:39:31 -0800

    no imagination

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,22 +11,16 @@
 */
 char *_strdup(char *str)
 {
-	char *copy;
-	int i, len = 0;
-
 	if (str == NULL)
 		return (NULL);
 
-	while (str[len] != '\0')
-		len++;
+	size_t length = strlen(str) + 1;
+	char *duplicate = (char *)malloc(length);
 
-	copy = (char *)malloc((sizeof(char) * len) + 1);
-	if (copy == NULL)
+	if (duplicate == NULL)
 		return (NULL);
 
-	for (i = 0; i < len; i++)
-		copy[i] = str[i];
-	copy[len] = '\0';
+	strcpy(duplicate, str);
 
-	return (copy);
+	return (duplicate);
 }
